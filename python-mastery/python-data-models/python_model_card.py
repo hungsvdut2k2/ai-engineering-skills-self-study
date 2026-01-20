10233cef255bf06c912a07fd4512a35f37b7d887
hung.nguyen-codelink <hungnguyen-codelink>
2026-01-20 19:55:45 +0700

    feat: init project structure

---
+++ python-mastery/python-data-models/python_model_card.py
@@ -0,0 +1,39 @@
+import collections
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+if __name__ == "__main__":
+    deck = FrenchDeck()
+    print(f"Total cards in deck: {len(deck)}")
+    print(f"First card in deck: {deck[0]}")
+    print(f"Last card in deck: {deck[-1]}")
+    print("A slice of the deck:", deck[12:16])
+    import random
+
+    print("A random card from the deck:", random.choice(deck))
+
+    sorted_deck = sorted(deck, key=spades_high)
+    print("Top 5 cards in sorted deck (spades high):", sorted_deck[-5:])
